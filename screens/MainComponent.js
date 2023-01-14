95e63c2397905ddcd43b5c9c10ea061ad5b861ad
bethwacker <116032081+bethwacker@users.noreply.github.com>
2023-01-14 12:06:22 -0500

    Workshop Assignment Jan 14

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -6,6 +6,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
+import AboutScreen from './AboutScreen';
+import ContactScreen from './ContactScreen';
+
 
 const Drawer = createDrawerNavigator();
 
@@ -50,9 +53,32 @@ const DirectoryNavigator = () => {
     );
 };
 
+const AboutNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='About'
+                component={AboutScreen}
+            />
+        </Stack.Navigator>
+    );
+};
+
+const ContactNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Contact'
+                component={ContactScreen}
+                options={{ title: 'Contact Us' }}
+            />
+        </Stack.Navigator>
+    );
+};
 const Main = () => {
     return (
-        <NavigationContainer>
         <View
             style={{
                 flex: 1,
@@ -74,9 +100,18 @@ const Main = () => {
                     component={DirectoryNavigator}
                     options={{ title: 'Directory' }}
                 />
+                <Drawer.Screen
+                    name='About'
+                    component={AboutNavigator}
+                   
+                />
+                <Drawer.Screen
+                    name='Contact'
+                    component={ContactNavigator}
+                    options={{ title: 'Contact Us' }}
+                />
             </Drawer.Navigator>
         </View>
-        </NavigationContainer>
     );
 };
 
